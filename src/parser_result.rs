4be42e8eb6799ff68a947e78b05fbb3f29607808
blck-snwmn <whitesnowmancreator@gmail.com>
2020-02-24 04:08:12 +0900

    Write comment and test

--- src/parser_result.rs
+++ src/parser_result.rs
@@ -1,13 +1,26 @@
+/// ParseResult is parse result for practice
+/// should use embedded Result
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum ParseResult<T> {
+    ///
     Success(T),
     Failure,
 }
 
 impl<T> ParseResult<T> {
+    /// Returns ParseResult::Success
+    /// ```
+    /// use parser_combinator::ParseResult;
+    /// assert_eq!(ParseResult::Success(10), ParseResult::success(10))
+    /// ```
     pub fn success(v: T) -> Self {
         Self::Success(v)
     }
+    /// Returns ParseResult::Failure
+    /// ```
+    /// use parser_combinator::ParseResult;
+    /// assert_eq!(ParseResult::<()>::Failure, ParseResult::failure())
+    /// ```
     pub fn failure() -> Self {
         Self::Failure
     }
